96ea96255c868f3894255c4daedf0d57c5ea9e55
henriquepuppoboueri <henrique.puppo@ufjf.br>
2022-08-30 18:58:38 -0300

    Commit final

---
+++ vuejs/teste/src/components/utils/QrEtiqueta.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { computed } from "vue";
+import VueQrious from "vue-qrious";
+import { useRoute } from "vue-router";
+
+const props = defineProps({ labelText: Object });
+</script>
+
+<template>
+  <div class="row inline q-ma-md">
+    <vue-qrious :value="`${label}`" level="H" :size="70"></vue-qrious>
+    <span class="input text-bold">
+      {{ labelText }}
+    </span>
+  </div>
+</template>
--- vuejs/teste/src/pages/PrintLabel.vue
+++ vuejs/teste/src/pages/PrintLabel.vue
@@ -2,19 +2,36 @@
 import { computed } from "vue";
 import VueQrious from "vue-qrious";
 import { useRoute } from "vue-router";
+import QrEtiqueta from "src/components/utils/QrEtiqueta.vue";
 
 const route = useRoute();
 const prefix = route.query.prefix;
-const sequence = route.query.start.padStart(5, 0);
+const firstSequence = route.query.start.padStart(5, 0);
+const pages = route.query.pages;
 
 const qrInput = computed(() => {
-  return `${prefix}${sequence}`;
+  return `${prefix}${firstSequence}`;
 });
 
-qrInput.value = route.query.start.padStart(5, 0);
+const amountOfLabels = computed(() => {
+  return pages * 50;
+});
 </script>
 
 <template>
-  <vue-qrious :value="qrInput" level="H"></vue-qrious>
-  {{ qrInput }}
+  <div class="container">
+    <div v-for="(label, index) in amountOfLabels" :key="index">
+      <qr-etiqueta :label-text="{ prefix, sequence: index }"></qr-etiqueta>
+    </div>
+  </div>
 </template>
+
+<style lang="sass">
+
+.input
+  margin: auto 0
+  font-size: 25px
+
+div
+  display: inline
+</style>
