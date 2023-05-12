e8fffb5f52d2219f6d825aeec3170b5e0da7b4da
UFJF - Henrique <henrique.puppo@ufjf.br>
2023-05-12 18:01:05 -0300

    Adição de filtros nas estatísticas diárias

--- vuejs/inventario/src/components/inventarios/estatisticas/StatsDia.vue
+++ vuejs/inventario/src/components/inventarios/estatisticas/StatsDia.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { onMounted, reactive, ref } from "vue";
+import { onMounted, reactive, ref, watch } from "vue";
 import { useRoute } from "vue-router";
 import { Line } from "vue-chartjs";
 import {
@@ -29,7 +29,8 @@ ChartJS.register(
 );
 
 const estatisticasStore = useEstatisticasStore();
-const { carregando, dados } = storeToRefs(estatisticasStore);
+const { carregando, dados, usuariosComColeta, diasComColeta } =
+  storeToRefs(estatisticasStore);
 const { buscarResumoDia } = estatisticasStore;
 const labels = [];
 const chartWidth = { type: Number, default: 400 };
@@ -37,35 +38,75 @@ const chartData = reactive({
   labels: labels,
   datasets: [],
 });
-
+const esconderUsuariosSemColeta = ref(false);
+const esconderDiasSemColeta = ref(false);
 const route = useRoute();
 const temDados = ref(false);
+
+watch(esconderUsuariosSemColeta, async (newV, old) => {
+  await buscarResumoDia(+route.params.idInventario);
+  montarGrafico(false, newV);
+});
+
+watch(esconderDiasSemColeta, async (newV, old) => {
+  await buscarResumoDia(+route.params.idInventario);
+  montarGrafico(newV, false);
+});
 
 onMounted(async () => {
   if ("idInventario" in route.params) {
     const id = +route.params.idInventario;
     await buscarResumoDia(id);
-
+    // await montarGrafico();
     temDados.value = !!dados.value.coleta.length;
-    chartData.datasets = dados.value.coleta.map((row) => ({
-      label: row.usuario.nome,
-      data: row.coleta.map((semana) => semana.qtde),
-      borderColor: gerarCorAleatoria(),
-      tension: 0.3,
-    }));
     if (temDados.value)
-      if (dados.value.coleta.length > 0)
-        dados.value.coleta[0].coleta.forEach((coletaDiaria) => {
-          labels.push(coletaDiaria.data);
-        });
+      if (dados.value.coleta.length > 0) {
+        montarGrafico();
+      }
   } else {
     return;
   }
 });
+
+async function montarGrafico(
+  esconderDiasSemColeta = false,
+  esconderUsuariosSemColeta = false
+) {
+  labels.splice(0, labels.length);
+  let dadosRelatorio = ref(null);
+
+  if (esconderUsuariosSemColeta) dadosRelatorio = usuariosComColeta;
+  else if (esconderDiasSemColeta) dadosRelatorio = diasComColeta;
+  else dadosRelatorio = dados;
+
+  chartData.datasets = await dadosRelatorio.value.coleta.map((row) => ({
+    label: row.usuario.nome,
+    data: row.coleta.map((semana) => semana.qtde),
+    borderColor: gerarCorAleatoria(),
+    tension: 0.3,
+  }));
+  for (
+    let index = 0;
+    index < dadosRelatorio.value.coleta[0].coleta.length;
+    index++
+  ) {
+    labels.push(dadosRelatorio.value.coleta[0].coleta[index].data);
+  }
+}
 </script>
 
 <template>
   <div class="col q-gutter-y-md" v-if="temDados">
-    <Line :width="chartWidth" :chart-data="chartData"></Line>
+    <Line :width="400" :height="300" :chart-data="chartData"></Line>
   </div>
+  <q-toggle
+    v-model="esconderUsuariosSemColeta"
+    color="green"
+    label="Esconder usuários sem coleta"
+  />
+  <q-toggle
+    v-model="esconderDiasSemColeta"
+    color="green"
+    label="Esconder dias sem coleta"
+  />
 </template>
--- vuejs/inventario/src/stores/estatisticas.js
+++ vuejs/inventario/src/stores/estatisticas.js
@@ -9,6 +9,43 @@ export const useEstatisticasStore = defineStore({
     erro: null,
     ultimaBusca: null,
   }),
+  getters: {
+    diasComColeta: (state) => {
+      const totaisDiarios = {}
+      const diasComColeta = []
+      state.dados.coleta.forEach((usuarioColetaDiaria) => {
+        usuarioColetaDiaria.coleta.forEach(coletaDia => {
+          totaisDiarios[coletaDia.data] = coletaDia.qtde + (totaisDiarios[coletaDia.data] || 0)
+        })
+      })
+      for (const key in totaisDiarios) {
+        if (totaisDiarios[key] > 0)
+          diasComColeta.push(key)
+      }
+      const response = {
+        ...state.dados, coleta: state.dados.coleta.map(usuColeta => {
+          return {
+            usuario: usuColeta.usuario,
+            coleta: usuColeta.coleta.filter(coletaDia => {
+              return diasComColeta.includes(coletaDia.data)
+            })
+          }
+        })
+      }
+      console.log(response);
+      return response
+    },
+    usuariosComColeta: (state) => {
+      const usuariosComColeta = state.dados.coleta.map((usuColeta) => {
+        return {
+          idUsuario: usuColeta.usuario.id,
+          totais: usuColeta.coleta.map(coletaSemana => coletaSemana.qtde).reduce((a, b) => a + b)
+        }
+      }).filter(usuColeta => usuColeta.totais > 0).map(usuario => usuario.idUsuario)
+
+      return { ...state.dados, coleta: state.dados.coleta.filter(usuColeta => usuariosComColeta.includes(usuColeta.usuario.id)) }
+    },
+  },
   actions: {
     async buscarTotaisSetores(idInventario) {
       try {
@@ -93,14 +130,23 @@ export const useEstatisticasStore = defineStore({
     async buscarResumoDia(idInventario) {
       try {
         this.carregando = true
-        const response = await api.get(`v1/restrito/resumo/obtemResumoPorDia/${idInventario}`)
-        const data = await response.data
+        const { data } = await api.get(`v1/restrito/resumo/obtemResumoPorDia/${idInventario}`)
         this.dados = await data
         this.carregando = false
-        this.ultimaBusca = { recalcular: this.recalcularResumoDia.bind(this, idInventario) }
+        this.ultimaBusca = { recalcular: this.recalcularResumoSemana.bind(this, idInventario) }
       } catch (error) {
         this.erro = error
       }
+      // try {
+      //   this.carregando = true
+      //   const response = await api.get(`v1/restrito/resumo/obtemResumoPorDia/${idInventario}`)
+      //   const data = await response.data
+      //   this.dados = await data
+      //   this.carregando = false
+      //   this.ultimaBusca = { recalcular: this.recalcularResumoDia.bind(this, idInventario) }
+      // } catch (error) {
+      //   this.erro = error
+      // }
     },
     async recalcularResumoDia(idInventario) {
       try {
