059f3961e01f90a31e61f123a2907c0be6298549
UFJF - Henrique <henrique.puppo@ufjf.br>
2023-05-31 18:59:54 -0300

    Iniciando migração para o Nuxt

---
+++ vuejs/inventario/src/components/LayoutHeader.vue
@@ -0,0 +1,24 @@
+<template>
+  <q-header elevated>
+    <q-toolbar>
+      <q-btn
+        flat
+        dense
+        round
+        icon="menu"
+        aria-label="Menu"
+        @click="toggleLeftDrawer"
+      />
+      <q-toolbar-title class="flex flex-center">
+        Controle de Inventário - UFJF
+      </q-toolbar-title>
+    </q-toolbar>
+  </q-header>
+</template>
+
+<script setup>
+import { ref } from "vue";
+const leftDrawerOpen = ref(true);
+</script>
+
+<style scoped></style>
